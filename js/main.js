19694469d9f9663ec19fa95b8f71da0cb590d178
Steven Huang <steven.charles.huang@gmail.com>
2018-07-17 01:28:31 -0400

    clickable model v1 complete

--- js/main.js
+++ js/main.js
@@ -1,16 +1,17 @@
 $(document).ready(() => {
   // console.log('jQuery ready!');
+  /* Nav MouseOver Events */
   $('.dropdown').on('mouseenter', () => {
     let dropdownGBCR = $('.dropdown')[0].getBoundingClientRect();
-    let dropdownGBCRRatio = Math.floor(dropdownGBCR.width / dropdownGBCR.height / 2)
+    let dropdownGBCRRatio = Math.floor(dropdownGBCR.width / dropdownGBCR.height) / 2;
     let dropdownWidth = Math.floor(dropdownGBCR.width) - dropdownGBCRRatio;
     let dropdownLeft = dropdownGBCR.left;
     let dropdownTop = dropdownGBCR.bottom;
-    console.log(dropdownGBCR);
-    console.log(dropdownGBCRRatio);
+
     $('.travel').css({
       'border-radius': '5px 5px 0 0'
     });
+
     $('.dropdown-content')
       .css({
         'display': 'block',
@@ -25,7 +26,8 @@ $(document).ready(() => {
     $('.dropdown-content')
       .css({
       });
-  })
+  });
+
   $('.dropdown').on('mouseleave', () => {
     $('.dropdown').css({
       'border-radius': '5px'
@@ -34,5 +36,65 @@ $(document).ready(() => {
       .css({
         'display': 'none'
     });
+
+  });
+
+  /* Nav MouseClick Events */
+  $('.about').on('click', () => {
+    $('main').html(`
+      <div class="about-content">
+        <h1>This is alllll about MisterKat</h1>
+      </div>
+    `);
   })
+
+  $('.recipes').on('click', () => {
+    $('main').html(`
+      <div class="recipes-content">
+        <h1>These are alllll the MisterKat Recipes</h1>
+      </div>
+    `);
+  })
+
+  /* MisterKat Adventures */
+  $('.local').on('click', () => {
+    $('main').html(`
+      <div class="recipes-content">
+        <h1>These are alllll the MisterKat Local adventures</h1>
+      </div>
+    `);
+  })
+
+  $('.east-coast').on('click', () => {
+    $('main').html(`
+      <div class="recipes-content">
+        <h1>These are alllll the MisterKat East Coast adventures</h1>
+      </div>
+    `);
+  })
+
+  $('.usa').on('click', () => {
+    $('main').html(`
+      <div class="recipes-content">
+        <h1>These are alllll the MisterKat USA adventures</h1>
+      </div>
+    `);
+  })
+
+  $('.international').on('click', () => {
+    $('main').html(`
+      <div class="recipes-content">
+        <h1>These are alllll the MisterKat International adventures</h1>
+      </div>
+    `);
+  })
+
+  $('.logo').on('click', () => {
+    $('main').html(`
+      <div class="hero">
+        Hello MisterKat!
+      </div>
+    `);
+  })
+
 });
